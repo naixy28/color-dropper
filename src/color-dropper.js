39076ed807b37092d551116cda48ac1f872a80af
ryuan018 <ryan.yuan@pwc.com>
2017-04-24 17:30:17 +0800

    add copy on click and validation

--- src/color-dropper.js
+++ src/color-dropper.js
@@ -1,10 +1,42 @@
 /**TODO
  * 
+ * - copy on click
  * 输入值的分析和过滤
+ * remove color
+ * persistence
  * 动画效果和样式
  * cloak
  * 分组？ 显示 隐藏
  */
+
+function selectText (element) {
+  var text = element,
+      range,
+      selection;
+
+  if (window.getSelection) {
+      selection = window.getSelection();
+      range = document.createRange();
+      range.selectNodeContents(text);
+      selection.removeAllRanges();
+      selection.addRange(range);
+  } else {
+      return false;
+  }
+}
+
+function validationColorNum ( newColor ) {
+    const isColor = /^#{0,1}([0-9a-fA-F]{3}|[0-9a-fA-F]{6})$/;
+    const addHash = /^(?!#)/;
+
+    var colorStr = newColor && newColor.trim();
+    if (!colorStr) return;
+    if (isColor.test(colorStr)){
+        colorStr = colorStr.toUpperCase().replace( addHash, '#' );
+        return colorStr;
+    }
+    return;
+}
 
 var app = new Vue({
 
@@ -26,6 +58,11 @@ var app = new Vue({
             };
             this.colors.push( { num: this.newColor } );
             this.newColor = '';
+        },
+        copyColor: function(colorNum, e) {
+            // TODO need validation on ele type
+            selectText(e.target);
+            document.execCommand('copy');
         }
     }
 })
